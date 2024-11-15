cc764475c9bacd920c0e84f15774118be9872f53
MohammedDaniyalRaza <160598244+MohammedDaniyalRaza@users.noreply.github.com>
2024-11-15 12:13:58 +0500

    Fixed double rendering issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 "use client"
-
 import React from "react";
-
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/Herosection";
 import ProjectSection from "./components/Project";
 import Skills from "./components/Skills";
 import ContactForm from "./components/Contact";
+
+
 
 export default function Home(){
     return(
